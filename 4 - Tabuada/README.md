0b259bf757c3d71952c95276e8354cde0bf468ef
João Victor <jvcgomes@gmail.com>
2024-05-29 23:05:10 -0300

    feat: adicionando o gerador de tabuada e seu README.md

---
+++ 4 - Tabuada/README.md
@@ -0,0 +1,4 @@
+### GERADOR DE TABUADA
+Um site que cria uma tabuada até 10 baseada no número que você informou. Ele não aceita o campo em branco. 
+
+![tabuada](md/tela-tabuada.PNG)
---
+++ 4 - Tabuada/script.js
@@ -0,0 +1,17 @@
+function gerar_tabuada() {
+    let numero_tabuada = document.getElementById("numero-tabuada").value;
+    let tabuada_resposta = document.getElementById("box-tabuada");
+
+    tabuada_resposta.innerHTML = "";
+
+    if (numero_tabuada == "") {
+        alert("Insira algum valor na caixa de entrada")
+
+    } else {
+        for (let i = 1; i <= 10; i++) {
+            let linha_tabuada = document.createElement("p");
+            linha_tabuada.textContent = `${numero_tabuada} x ${i} = ${numero_tabuada * i}`;
+            tabuada_resposta.appendChild(linha_tabuada);
+        }
+    }
+}
